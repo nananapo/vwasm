9273ff01ae9c4a7cb5f2833158174a1c52c5c65c
kanataso <kanapodream@gmail.com>
2023-02-19 03:19:13 +0900

    32bit単位なので、アドレスを2bit右シフトするように変更

--- src/memory.v
+++ src/memory.v
@@ -13,15 +13,22 @@ module Memory #(
 	input  wire [31:0]	wdata
 );
 
+// memory
 reg [31:0] data_mem [MEMORY_SIZE-1:0];
 
+// status
 localparam STATE_WAIT = 0;
 localparam STATE_READ = 1;
 localparam STATE_WRITE = 1;
 
 reg [3:0]	state = STATE_WAIT;
+
+// saved input
 reg [31:0]	addr_bup;
 reg [31:0]	wdata_bup;
+
+// shidted address
+wire [31:0] addr_shift = addr_bup >> 2;
 
 assign cmd_ready = state == STATE_WAIT;
 
@@ -33,15 +40,15 @@ always @(posedge clk) begin
 		rdata_ready	<= 0;
 	end else if (state == STATE_READ) begin
 		rdata <= {
-			data_mem[addr_bup][7:0],
-			data_mem[addr_bup][15:8],
-			data_mem[addr_bup][23:16],
-			data_mem[addr_bup][31:24]
+			data_mem[addr_shift][7:0],
+			data_mem[addr_shift][15:8],
+			data_mem[addr_shift][23:16],
+			data_mem[addr_shift][31:24]
 		};
 		rdata_ready <= 1;
 		state <= STATE_WAIT;
 	end else if (state == STATE_WRITE) begin
-		data_mem[addr_bup] = {
+		data_mem[addr_shift] = {
 			wdata[7:0],
 			wdata[15:8],
 			wdata[23:16],
