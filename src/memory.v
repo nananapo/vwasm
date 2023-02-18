87c79ab16b9cecaf6b514f258a3fd513807ad0a4
kanataso <kanapodream@gmail.com>
2023-02-19 05:53:55 +0900

    4byteアラインされていないアドレスへの書き込みのサポートとバグ修正
    読み込み時にエンディアンがおかしくなっていたのと、保存済みのアドレスを使っていなかったのを修正

--- src/memory.v
+++ src/memory.v
@@ -35,6 +35,7 @@ assign cmd_ready = state == STATE_WAIT;
 // アドレスが4byte alignedでは無い場合用
 reg [3:0]	clock_cnt;
 reg [31:0]	data_save;
+reg [31:0]	data_save2;
 
 always @(posedge clk) begin
 	if (state == STATE_WAIT && cmd_start == 1) begin
@@ -44,7 +45,7 @@ always @(posedge clk) begin
 		clock_cnt	<= 0;
 		rdata_ready	<= 0;
 	end else if (state == STATE_READ) begin
-		if (addr % 4 == 0) begin
+		if (addr_bup % 4 == 0) begin
 			rdata <= {
 				data_mem[addr_shift][7:0],
 				data_mem[addr_shift][15:8],
@@ -55,21 +56,15 @@ always @(posedge clk) begin
 			state <= STATE_WAIT;
 		end else begin
 			if (clock_cnt == 0) begin
-				data_save <= {
-					data_mem[addr_shift][7:0],
-					data_mem[addr_shift][15:8],
-					data_mem[addr_shift][23:16],
-					data_mem[addr_shift][31:24]
-				};
+				data_save <= data_mem[addr_shift];
 				clock_cnt <= clock_cnt + 1;
-				state <= STATE_WAIT;	
 			end else begin
 				rdata <= {
-					addr % 4 == 1 ? {
+					addr_bup % 4 == 1 ? {
 							data_mem[addr_shift][31:24],
 							data_save[31:8]
 						} :
-					addr % 4 == 2 ? {
+					addr_bup % 4 == 2 ? {
 							data_mem[addr_shift][23:16],
 							data_mem[addr_shift][31:24],
 							data_save[31:16]
@@ -86,15 +81,42 @@ always @(posedge clk) begin
 			end
 		end
 	end else if (state == STATE_WRITE) begin
-		data_mem[addr_shift] = {
-			wdata[7:0],
-			wdata[15:8],
-			wdata[23:16],
-			wdata[31:24]
-		};
-		rdata_ready <= 1;
-		state <= STATE_WAIT;
+		if (addr_bup % 4 == 0) begin
+			data_mem[addr_shift] = {
+				wdata[7:0],
+				wdata[15:8],
+				wdata[23:16],
+				wdata[31:24]
+			};
+			rdata_ready <= 1;
+			state <= STATE_WAIT;
+		end else begin
+			if (clock_cnt == 0) begin
+				data_save <= data_mem[addr_shift];
+				clock_cnt <= clock_cnt + 1;
+			end else if (clock_cnt == 1) begin
+				data_save2 <= data_mem[addr_shift + 1];
+				clock_cnt <= clock_cnt + 1;
+			end else if (clock_cnt == 2) begin
+				data_mem[addr_shift] <= {
+					addr_bup == 1 ?
+						{data_save[31:24], wdata_bup[7:0], wdata_bup[15:8], wdata_bup[23:16]} : 
+					addr_bup == 2 ?
+						{data_save[31:16], wdata_bup[7:0], wdata_bup[15:8]} : 
+					{data_save[31:8], wdata_bup[7:0]}
+				};
+				clock_cnt <= clock_cnt + 1;
+			end else begin
+				data_mem[addr_shift + 1] <= {
+					addr_bup == 1 ?
+						{wdata_bup[31:24], data_save2[23:0]} : 
+					addr_bup == 2 ?
+						{wdata_bup[31:24], wdata_bup[23:16], data_save2[15:0]} : 
+					{wdata_bup[31:24], wdata_bup[23:16], wdata_bup[15:8], data_save2[7:0]}
+				};
+				clock_cnt <= clock_cnt + 1;
+			end
+		end
 	end
 end
-
 endmodule
