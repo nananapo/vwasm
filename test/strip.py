4415b38fe9b61c77d53e29335c2ce5dfa0952809
nananapo <kanapodream@gmail.com>
2023-02-20 00:14:21 +0900

    あああ

--- test/strip.py
+++ test/strip.py
@@ -7,9 +7,9 @@ while True:
     # 00000000: 037f010060010501000000016d736100  .asm.......`....
     start = len("00000000: ")
     end = start + len("037f010060010501000000016d736100")
-    S = S[start:end + 1]
+    S = S[start:end]
     
-    print(S[24:31].replace(" ", "0"))
-    print(S[16:23].replace(" ", "0"))
-    print(S[8 :15].replace(" ", "0"))
-    print(S[0 : 7].replace(" ", "0"))
+    print(S[24:32].replace(" ", "0"))
+    print(S[16:24].replace(" ", "0"))
+    print(S[8 :16].replace(" ", "0"))
+    print(S[0 : 8].replace(" ", "0"))
