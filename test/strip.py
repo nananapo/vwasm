4b491cd12c85825ec77cbec46140eddaad1f2104
na <kanapodream@gmail.com>
2023-02-19 17:50:58 +0900

    メモリに書き込む用のファイルを作成するMakefileを作成

---
+++ test/strip.py
@@ -0,0 +1,15 @@
+while True:
+    S = None
+    try:
+        S = input()
+    except:
+        exit()
+    # 00000000: 037f010060010501000000016d736100  .asm.......`....
+    start = len("00000000: ")
+    end = start + len("037f010060010501000000016d736100")
+    S = S[start:end]
+    
+    print(S[24:31].replace(" ", "0"))
+    print(S[16:23].replace(" ", "0"))
+    print(S[8 :15].replace(" ", "0"))
+    print(S[0 : 7].replace(" ", "0"))
